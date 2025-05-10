fced99418b6ed771f8d24be58b3186a8514531ac
Shaan6695 <sam33@njit.edu>
2025-05-10 11:54:31 -0400

    initial project setup

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CreateTrip from './create-trip/index.jsx'
+import { Toaster } from './components/ui/sonner.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Viewtrip from './view-trip/[tripId]/index.jsx'
+import MyTrips from './my-trips/index.jsx'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />
+},
+{
+  path:'/create-trip',
+  element: <CreateTrip />
+},
+{
+  path: '/view-trip/:tripId',
+  element: <Viewtrip />
+},
+{
+  path: '/my-trips',
+  element: <MyTrips />
+}
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <Toaster/>
+      <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
